0774c197710ede80fda6c79fae992aaf6463b1c6
Tatiana <Rybalkinatr@gmail.com>
2023-03-03 10:53:10 +0400

    Lesson5.1

---
+++ Lesson5.1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Вывод двумерного массива
+
+string[,] table = new string[2, 3];
+table[1, 2] = "слово";
+for (int rows = 0; rows < 2; rows++)
+{
+    for (int columns = 0; columns < 3; columns++)
+    {
+        Console.WriteLine($"-{table[rows, columns]}-");
+    }
+}
